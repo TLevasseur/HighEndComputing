e8bc8f40b8293e5f5b3f70adec81b706841e70fd
QuentinDiaferia <q.diaferia@gmail.com>
2016-03-03 16:24:49 +0000

    Config file - server side

--- development/meshslicer/configfile.php
+++ development/meshslicer/configfile.php
@@ -1,8 +1,23 @@
 <?php
-header('Location: dashboard.php');
+session_start();
 
+include('Net/SFTP.php');
 
+$str = $_GET['str'];
 
+$sftp = new Net_SFTP('hpclogin-1.central.cranfield.ac.uk');
+if ($sftp->login($_SESSION['id'], $_SESSION['passwd'])) {
 
+	file_put_contents('uploads/config.ini', $str);
+	$sftp->put('/scratch/'.$_SESSION["id"].'/meshslicer/config.ini', 'uploads/config.ini' , NET_SFTP_LOCAL_FILE);
 
+	$reply = json_encode(array('Error' => '0', 'Message' => ""));
+	unlink('uploads/config.ini');
+
+}
+else { 
+	$reply = json_encode(array('Error' => '1', 'Message' => "Astral problem. Please try again."));
+}
+
+echo $reply;
 ?>
