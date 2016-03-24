aa47152e83eb172f350bdc37d8a003b4f315eef0
TLevasseur <levasseur.thibaud@gmail.com>
2016-03-24 16:17:17 +0000

    working now lol *pan*

--- development/meshslicer/dashboard.php
+++ development/meshslicer/dashboard.php
@@ -593,14 +593,19 @@ function setEvents() {
                         o,
                         N = Nodes.length,
                         E = 45000,
-                        C = parseInt(element.parent().prev().text().split(".parts.")[1].split('.')[0]),
                         d = 0.5,
                         cs = [],
                         g = {
                           nodes: [],
                           edges: []
                         };
-
+                        if(element.parent().prev().text().indexOf("eparts") > -1){
+                            C = parseInt(element.parent().prev().text().split(".eparts.")[1].split('.')[0]);
+                        }else if(element.parent().prev().text().indexOf("nparts") > -1){
+                            C = parseInt(element.parent().prev().text().split(".nparts.")[1].split('.')[0]);
+                        }else{
+                            C = parseInt(element.parent().prev().text().split(".parts.")[1].split('.')[0]);
+                        }
                         for (i = 0; i < C; i++)
                           cs.push({
                             id: i,
