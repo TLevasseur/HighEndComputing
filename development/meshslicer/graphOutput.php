d2c5193165fc7be030e776643626d4fc74d359cd
TLevasseur <levasseur.thibaud@gmail.com>
2016-03-24 15:31:10 +0000

    Duplicate graph work on meshes

--- development/meshslicer/graphOutput.php
+++ development/meshslicer/graphOutput.php
@@ -6,8 +6,16 @@ include('Net/SFTP.php');
 
 $fileToGet = $_GET['file'];
 $graphPartition = 'uploads/'.$fileToGet;
-$tmp=explode(".parts.", $fileToGet, 2)[0];
-$initialGraph = 'uploads/'.substr($tmp,0,strrpos($tmp, '.'));
+if(strpos($fileToGet, '.nparts.') !== false){
+    $tmp=explode(".out.nparts.", $fileToGet, 2)[0];
+    $initialGraph = 'uploads/'.substr($tmp,0,strrpos($tmp, '.'));
+}else if(strpos($fileToGet, '.eparts.') !== false){
+    $tmp=explode(".out.eparts.", $fileToGet, 2)[0];
+    $initialGraph = 'uploads/'.substr($tmp,0,strrpos($tmp, '.'));
+}else{
+    $tmp=explode(".parts.", $fileToGet, 2)[0];
+    $initialGraph = 'uploads/'.substr($tmp,0,strrpos($tmp, '.'));
+}
 $statsResults = 'uploads/'.$fileToGet.'.stats';
 
 $sftp = new Net_SFTP(SSH_HOST);
@@ -20,16 +28,83 @@ if ($sftp->login($_SESSION['id'], $_SESSION['passwd'])) {
     $rows        = explode("\n", $txt_file);
     $N =intval(explode(" ",array_shift($rows))[0]);
     $Nodes=[];
+    if(strpos($fileToGet, '.nparts.') !== false){
+        $txt_file    = file_get_contents($graphPartition);
+        $colorRows= explode("\n", $txt_file);
+        $color=[];
+        $max=0;
+        for($i=1;$i<=$N;$i++){
+          $r=explode(" ",$rows[$i]);
+          foreach ($r as $k => $v) {
+            $val=intval($v);
+            if($val>$max)$max=$val;
+            foreach ($r as $k2 => $v2) {
+                
+                $val2=intval($v2);
 
-    //Initial Set
-    for($i=1;$i<=$N;$i++){
-        $edges=[];
-        $explodedLine=explode(" ",array_shift($rows));
-        for($j=count($explodedLine)-1; $j>0; $j--)
-            array_push($edges, intval($explodedLine[$j]));
-        unset($edges[0]);
-        $Nodes[$i]=$edges;
+                if($val>0 && $val2>0 && $k2>$k){
+                  if(!isset($Nodes[$val])){
+                    $Nodes[$val]=[];
+                  }
+                  if(!isset($Nodes[$val2])){
+                    $Nodes[$val2]=[];
+                  }
+                  array_push($Nodes[$val],$val2);
+                  array_push($Nodes[$val2],$val);
+                  $Nodes[$val]=array_unique($Nodes[$val]);
+                  $Nodes[$val2]=array_unique($Nodes[$val2]);
+                }
+            }
+          }
+        }
+        for($i=0;$i<$max;$i++)
+          $color[$i+1]=intval($colorRows[$i]);
+    }else if(strpos($fileToGet, '.eparts.') !== false){
+        $txt_file    = file_get_contents($graphPartition);
+        $colorRows= explode("\n", $txt_file);
+        $color=[];
+        //Initial Set
+        for($i=1;$i<=$N;$i++){
+          $r=explode(" ",$rows[$i]);
+          foreach ($r as $k => $v) {
+            $val=intval($v);
+            $color[$i]=intval($colorRows[$i-1]);
+            foreach ($r as $k2 => $v2) {
+                
+                $val2=intval($v2);
+
+                if($val>0 && $val2>0 && $k2>$k){
+                  if(!isset($Nodes[$val])){
+                    $Nodes[$val]=[];
+                  }
+                  if(!isset($Nodes[$val2])){
+                    $Nodes[$val2]=[];
+                  }
+                  array_push($Nodes[$val],$val2);
+                  array_push($Nodes[$val2],$val);
+                  $Nodes[$val]=array_unique($Nodes[$val]);
+                  $Nodes[$val2]=array_unique($Nodes[$val2]);
+                }
+            }
+          }
+        }
+    }else{
+        //Initial Set
+        for($i=1;$i<=$N;$i++){
+            $edges=[];
+            $explodedLine=explode(" ",array_shift($rows));
+            for($j=count($explodedLine)-1; $j>0; $j--)
+                array_push($edges, intval($explodedLine[$j]));
+            unset($edges[0]);
+            $Nodes[$i]=$edges;
+        }
+        $txt_file    = file_get_contents($graphPartition);
+        $rows        = explode("\n", $txt_file);
+        $color=[];
+        for($i=0;$i<$N;$i++) $color[$i]=intval($rows[$i]);
     }
+
+
 
     while(count($Nodes)>1500){
     	$key=array_rand($Nodes);
@@ -50,10 +125,7 @@ if ($sftp->login($_SESSION['id'], $_SESSION['passwd'])) {
     	//echo "<br>";
     }
 
-    $txt_file    = file_get_contents($graphPartition);
-    $rows        = explode("\n", $txt_file);
-    $color=[];
-    for($i=0;$i<$N;$i++) $color[$i]=intval($rows[$i]);
+
 
     // STATS
     $statFile = file($statsResults);
